fd86a0cf7e8afdb96a5457a16fd56ae7db878922
Chandan Mourya <chandanmourya064@gmail.com>
2026-07-11 16:27:47 +0530

    Commit all

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import HomaPage from './landing_pages/home/HomePage';
+import Signup from './landing_pages/signup/Signup';
+// import About from './landing_pages/about/AboutPage';
+import AboutPage from './landing_pages/about/AboutPage';
+import ProductPage from './landing_pages/products/ProductPage'
+import PricingPage from './landing_pages/pricing/PricingPage';
+import SupportPage from './landing_pages/supports/SupportPage';
+import Navbar from './landing_pages/Navbar';
+import Footer from './landing_pages/Footer';
+import NotFound from './landing_pages/NotFound';
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <div><h1>Hello World</h1></div>
-  </React.StrictMode>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<HomaPage/>}></Route>
+      <Route path="/signup" element={<Signup/>}></Route>
+      <Route path="/about" element={<AboutPage/>}></Route>
+      <Route path="/product" element={<ProductPage/>}></Route>
+      <Route path="/pricing" element={<PricingPage/>}></Route>
+      <Route path="/support" element={<SupportPage/>}></Route>
+      <Route path="/*" element={<NotFound/>}></Route>
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
 );
 
 
